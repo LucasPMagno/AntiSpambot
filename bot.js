520a06cfefeff0d3f4ed42d4d13588de3367ed7a
LucasPMagno <lucaspmagno@gmail.com>
2017-05-25 16:53:59 -0400

    Fixed some stuff
    and gave myself credits :v
    Thank goodness this command is not 3830 files long :)

--- bot.js
+++ bot.js
@@ -27,11 +27,11 @@ bot.on('guildMemberAdd', member => {
 bot.on('message', message => {
     if (bot.user === message.author) return;
     if (message.channel.type !== 'dm') return;
+    if (bot.guilds.find('id', config.guildID).members.get(message.author.id).roles.get(config.roleID)) return;
     if (message.content.includes('yes')) {
-        console.log('1');
-        bot.guilds.find('id', config.guildID).members.find('id', message.author.id).addRole(config.roleID);
+        bot.guilds.find('id', config.guildID).members.get(message.author.id).addRole(config.roleID);
     } else if (message.content.includes('no') && !message.author.roles) {
-        bot.guilds.find('id', config.guildID).members.find('id', message.author.id).kick()
+        bot.guilds.find('id', config.guildID).members.get(message.author.id).kick()
     }
 });
 bot.login(config.token);
