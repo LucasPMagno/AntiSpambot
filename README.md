127b462306136aec3bb4ca047921bc8499e21a94
LucasPMagno <lucaspmagno@gmail.com>
2017-05-26 18:41:29 -0400

    Added a crap ton of stuff
    Added eval command
    Added unverify command
    improved verification of members
    Added response to yes or no
    Now accepts lower and upper case
    Updated .gitignore to actually ignore mdoules
    Give credits to who made it... Nate!

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # Anti Spam Bot
 ### (User PM Authentication)
 
+## Credits
+Thanks to LucasPMagno for writing this bot and then adding more to it! I also thank him for being lazy and not putting it on GitHub so that I could have all the credit for me before he edited it.
+
 ## Synopsis
-Basically, this bot allows you to prevent spammers and userbots on Discord. It also makes users more likely to actuslly read the rules. The bot DMs them when they join the server, and then they have to send the bot "yes". That would be like at the bottom of rules, as the message the bot DMs them with says "Please read the rules on the server!", and at the bottom of the rules eould be "DM the bot "yes" to be grsnted talk power. once you reply with "yes, the bot gives you a role (cudtomizeable). If you say "no" or anything else, it kicks you.
+Basically, this bot allows you to prevent spammers and userbots on Discord. It also makes users more likely to actually read the rules. The bot DMs them when they join the server, and then they have to send the bot "yes". Once you reply with "yes, the bot gives you a specified role. If you say "no", it kicks you.
 
 ## Motivation
-This project happened because I was fed up eith scumbags coming on my server and instantly sending messages on how we should join their "amazing" server. So, I began this project.
+This project happened because I was fed up eith scumbags coming on my server and instantly sending messages on how we should join their "amazing" server. So, I asked LucasPMagno to do this for me.
 
 ## Installation
 Installation is easy. Just open up command prompt and follow these steps:
 * Type "git clone https://github.com/ACA30/AntiSpambot.git"
 * Type "cd AntiSpambot"
 * Type "yarn" if this command doesn't work, type "npm install --global yarn". If that command doesn't work, please submit an issue with this repo and we will send help.
-* Type "yarn init"
-* Hit enter until the questions stop coming.
-* Type "yarn start". If that doesn't work, type "node bot.js"
-* Copy the invite link to your bot, and you're good to go.
+* Type "yarn start".
 * NOTE: Sadly, you can't fully close the terminal window after starting your bot. You can minimize it.
 
 ## Activation
@@ -25,6 +25,5 @@ To activate your bot if it goes offline, follow these steps.
 * Enjoy!
 
 ## API Reference
-Our bot uses the following to work:
-* NodeJS
-* Discord.js
+Our bot needs the following to work:
+* `node.js`
--- bot.js
+++ bot.js
@@ -1,21 +1,112 @@
 const Discord = require('discord.js');
+const RichEmbed = require('discord.js').RichEmbed
 const bot = new Discord.Client();
 const config = require('./config.json')
 
 bot.on('ready', () => {});
 
+function rule(sendto) {
+    sendto.send('Welcome to **NateDev - Official**! \nYou must agree to the rules! \n*Rules:* \nThis is a public discord server. These rules always apply, and they can and will be changed by senior staff members at any time. Please abide by these rules at all times. \n:one: You may not have an inappropriate Discord username, profile picture or game. You will be asked 2 times to change it, and then will be kicked. \n:two: You may not use sexual, homophobic, or derogatory slurs. You will warned up to three times, then you will muted. \n:three: No threats are allowed, even if the user was okay with it. You will be instantly banned without any further notice. \n:four: You are not to post links to anything unrelated to Minecraft, Bukkit, Spigot, or Nate unless it is in #offtopic-and-bot-tests. You will be warned up to a couple of times. However, if it is an inappropriate, you will instantly kicked. \n:five: You are not to excessively use caps or spam characters. You will be warned up to a couple of times. \n:six: You are not to talk back or talk smack about staff members. Even if it was a joking matter, you will be warned, then muted. \n:seven: Keep swearing to a minimum, and keep your conversations PG-13. It is entirely unnecessary, and not everyone appreciates it. \n:eight: Keep advertisements for Minecraft servers, Discord servers, or any other service in #promote-yourself: \n:white_small_square: Only promote yourself or your services at most once every 24 hours. \n:white_small_square: Do not try and steal my requests / server members. ("My server is better than Nates!!") \n:white_small_square: Do not advertise paid products or paid services. \n:white_small_square: You must be at least level 5 to post any promotions. (-rank to check your level) \n:white_check_mark: If you believe any of these rules are unfair, please let me know in #discord-suggestions! \n \nIf you agree to these rules, please type `yes` in this DM. If not, type `no`')
+}
+
+exports.multiSendCode = (channel, messages, delay) => {
+    delay = delay || 100;
+    messages.forEach((m, i) => {
+        setTimeout(() => {
+            channel.send('\`\`\`js\n' + m + '\`\`\`');
+        }, delay * i);
+    });
+};
+
+exports.sendLarge = (channel, largeMessage, options = {}) => {
+    let message = largeMessage;
+    let messages = [];
+    let prefix = options.prefix || '';
+    let suffix = options.suffix || '';
+
+    let max = 1989 - prefix.length - suffix.length;
+
+    while (message.length >= max) {
+        let part = message.substr(0, max);
+        let cutTo = max;
+        if (options.cutOn) {
+            cutTo = part.lastIndexOf(options.cutOn);
+            part = part.substr(0, cutTo);
+        }
+        messages.push(prefix + part + suffix);
+        message = message.substr(cutTo);
+    }
+
+    if (message.length > 1) {
+        messages.push(prefix + message + suffix);
+    }
+
+    this.multiSendCode(channel, messages, options.delay);
+};
+
+exports.clean = (text) => {
+    if (typeof (text) === 'string') {
+        return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203)).replace(config.token, 'BOT_TOKEN' + String.fromCharCode(8203));
+    }
+    else {
+        return text;
+    }
+}
+
 bot.on('guildMemberAdd', member => {
-    member.send('Welcome to **NateDev - Official**! \nYou must agree to the rules! \n*Rules:* \nThis is a public discord server. These rules always apply, and they can and will be changed by senior staff members at any time. Please abide by these rules at all times. \n:one: You may not have an inappropriate Discord username, profile picture or game. You will be asked 2 times to change it, and then will be kicked. \n:two: You may not use sexual, homophobic, or derogatory slurs. You will warned up to three times, then you will muted. \n:three: No threats are allowed, even if the user was okay with it. You will be instantly banned without any further notice. \n:four: You are not to post links to anything unrelated to Minecraft, Bukkit, Spigot, or Nate unless it is in #offtopic-and-bot-tests. You will be warned up to a couple of times. However, if it is an inappropriate, you will instantly kicked. \n:five: You are not to excessively use caps or spam characters. You will be warned up to a couple of times. \n:six: You are not to talk back or talk smack about staff members. Even if it was a joking matter, you will be warned, then muted. \n:seven: Keep swearing to a minimum, and keep your conversations PG-13. It is entirely unnecessary, and not everyone appreciates it. \n:eight: Keep advertisements for Minecraft servers, Discord servers, or any other service in #promote-yourself: \n:white_small_square: Only promote yourself or your services at most once every 24 hours. \n:white_small_square: Do not try and steal my requests / server members. ("My server is better than Nates!!") \n:white_small_square: Do not advertise paid products or paid services. \n:white_small_square: You must be at least level 5 to post any promotions. (-rank to check your level) \n:white_check_mark: If you believe any of these rules are unfair, please let me know in #discord-suggestions! \n \nIf you agree to these rules, please type `yes` in this DM. If not, type `no`');
+    rule(member);
 });
 
 bot.on('message', message => {
     if (bot.user === message.author) return;
     if (message.channel.type !== 'dm') return;
-    if (bot.guilds.find('id', config.guildID).members.get(message.author.id).roles.get(config.roleID)) return;
-    if (message.content.includes('yes')) {
-        bot.guilds.find('id', config.guildID).members.get(message.author.id).addRole(config.roleID);
-    } else if (message.content.includes('no') && !message.author.roles) {
-        bot.guilds.find('id', config.guildID).members.get(message.author.id).kick('You have been kicked because you did not agree to the rules!')
+    if (bot.guilds.get(config.guildID).members.get(message.author.id).roles.get(config.roleID)) return;
+    if (message.content.toLowerCase().includes('yes')) {
+        bot.guilds.get(config.guildID).members.get(message.author.id).addRole(config.roleID).catch(console.error);
+        message.channel.send(':white_check_mark: You are now verified on ' + bot.guilds.get(config.guildID).name + '.');
+    } else if (message.content.toLowerCase().includes('no') && !message.author.roles) {
+        bot.guilds.get(config.guildID).members.get(message.author.id).kick('You have been kicked because you did not agree to the rules!').catch(console.error);
+        message.channel.send(':x: You were kicked from ' + bot.guilds.get(config.guildID).name + '.');
     }
 });
+
+bot.on('message', message =>{
+    if (!message.content.startsWith(config.prefix)) return;
+    if (message.author.bot) return;
+    let command = message.content.split(' ')[0].slice(config.prefix.length);
+    let args = message.content.split(' ').slice(1);
+    if (command === 'help') {
+        message.channel.send({
+            embed: new RichEmbed()
+            .addField(':wrench: Utility:', `\`${config.prefix}eval\`: Evaluates JavaScript code.`)
+            .addField(':shield: Moderation:', `\`${config.prefix}unverify\`: Forces the user to read the rules again`)
+        });
+    }
+
+    if (command === 'eval') {
+        if (message.author.id !== '263118500724342784') return;
+        if (args < 1) {
+            return message.channel.send(':x: Please provide code to evaluate!');
+        }
+        try {
+            let evaled = eval(args.join(' '));
+            let output = require('util').inspect(evaled);
+            let clean = this.clean(output);
+            this.sendLarge(message.channel, clean);
+        } catch (err) {
+            this.sendLarge(message.channel, err);
+        }
+    }
+
+    if (command === 'unverify') {
+        if (message.mentions) {
+            return message.channel.send(':x: Please mention a member!');
+        }
+        let user = message.guild.member(message.mentions.users.first());
+        user.removeRole(config.roleID);
+        rule(user);
+        message.channel.send(':white_check_mark: Unverified user.').then(m => m.delete(5000));
+    }
+});
+
 bot.login(config.token);
